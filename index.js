22a67ca720586dc7663cc7f5bd646008b1f393f1
hyuntae94 <believe4279@gmail.com>
2020-07-24 03:26:23 +0900

    utube

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 
 const PORT = 7000;
@@ -9,11 +13,11 @@ const handleHome = (req, res) => res.send("Main Page");
 
 const handleProfile = (req, res) => res.send("profile page");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-};
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get('/', handleHome);
 

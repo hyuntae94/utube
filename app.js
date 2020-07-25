6c361529854682f61d4043299fee700b920cbcbb
hyuntae94 <believe4279@gmail.com>
2020-07-25 21:12:42 +0900

    utube

--- app.js
+++ app.js
@@ -3,15 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-const PORT = 7000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send("Main Page");
-
-const handleProfile = (req, res) => res.send("profile page");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -19,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get('/', handleHome);
+app.use("/",globalRouter);
+app.use("/user", userRouter);
+app.use("/video",videoRouter);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;

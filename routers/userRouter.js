ec1abc22f01bec06d40f080929a31473211cdf84
hyuntae94 <believe4279@gmail.com>
2020-07-28 16:05:25 +0900

    utube

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
+import {
+    userDetail,
+    editProfile,
+    changePassword,
+    users
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import { videos, videoDetail, editVideo, deleteVideo, upload } from "../controllers/videoController";
+import { videoDetail, editVideo, deleteVideo, upload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);

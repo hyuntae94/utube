31ceff02d5d6f1fdbdf6160ab23f3934e06d8649
hyuntae94 <believe4279@gmail.com>
2020-08-05 17:37:51 +0900

    Search

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,11 +11,19 @@ export const home = async (req, res) => {
     }
 }
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {
         query: { term: searchingBy }
     } = req;
-    res.render("search", { pageTitle: "Search", searchingBy, videos });
+    let videos = [];
+    try {
+        videos = await Video.find({
+            title: { $regex: searchingBy, $options: "i" }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    res.render("search", { pageTitle: "Search", searchingBy, videos })
 };
 
 export const getUpload = (req, res) =>
@@ -88,6 +96,8 @@ export const deleteVideo = async (req, res) => {
     } = req;
     try {
         await Video.findOneAndRemove({ _id: id });
-    } catch (error) { }
+    } catch (error) {
+        console.log(error);
+    }
     res.redirect(routes.home);
 }

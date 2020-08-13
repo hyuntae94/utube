ed7e9b7ee9723a4ac0a487646abacae89e3dd883
hyuntae94 <believe4279@gmail.com>
2020-08-14 02:02:04 +0900

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 }
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
     const {
         body: { name, email, password, password2 }
     } = req;
@@ -12,6 +13,16 @@ export const postJoin = (req, res) => {
         res.status(400);
         res.render("Join", { pageTitle: "Join" });
     } else {
+        try {
+            const user = await User({
+                name,
+                email
+                //req.body의 name,email인가 UserSchema의 name,email인가
+            });
+            await User.register(user, password);
+        } catch (error) {
+            console.log(error);
+        }
         res.redirect(routes.home);
     }
 }

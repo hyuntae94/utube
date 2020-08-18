90f527529cbfcf21d9d4642b4f07194168926819
hyuntae94 <believe4279@gmail.com>
2020-08-18 18:11:03 +0900

    GitHub Login Part1

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,12 @@ export const postLogin = passport.authenticate("local", {
     successRedirect: routes.home
 });
 
+export const githubLoginCallback = (accessToken, refreshToken, profile, cb) => {
+    console.log(accessToken, refreshToken, profile, cb);
+}
+
 export const logout = (req, res) => {
+    req.logout();
     res.redirect(routes.home);
 };
 
--- passport.js
+++ passport.js
@@ -1,7 +1,18 @@
 import passport from "passport";
 import User from "./models/User";
+import GithubStrategy from "passport-github";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+passport.use(
+	new GithubStrategy({
+		clientID: process.env.GH_ID,
+		clientSecret: process.env.GH_SECRET,
+		callbackURl: "http://localhost:3333/auth/github/callback"
+	}),
+	githubLoginCallback,
+);
